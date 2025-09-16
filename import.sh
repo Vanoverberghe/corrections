e12832dd0578206df5729919885784c9fecf3050
Vanoverberghe <vano.timothe@gmail.com>
2025-09-16 14:58:20 +0200

    Maj

--- import.sh
+++ import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp -r /mnt/c/Users/vanot/Desktop/corrections/NSI_1ere ./NSI_1ere
+cp -r /mnt/c/Users/vanot/Desktop/corrections/NSI_1ere ./
 
-cp -r /mnt/c/Users/vanot/Desktop/corrections/SNT ./SNT
+cp -r /mnt/c/Users/vanot/Desktop/corrections/SNT ./
 
-cp -r /mnt/c/Users/vanot/Desktop/corrections/NSI_Tale ./NSI_Tale
+cp -r /mnt/c/Users/vanot/Desktop/corrections/NSI_Tale ./
